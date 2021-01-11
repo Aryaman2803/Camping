4592fd287f0fce2c03789c91ff614efe7e5c1cb4
aryaman2803 <aryamansingh2803@gmail.com>
2021-01-12 00:11:15 +0530

    initial

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 3000;
+const Campground = require("./models/campground");
+const methodOverride = require("method-override");
+
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection error:"));
+db.once("open", () => {
+  console.log("Database Connected!");
+});
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+//(1) Home
+app.get("/", (req, res) => {
+  res.render("home");
+});
+//(2) Campground Index
+app.get("/campgrounds", async (req, res) => {
+  const campgrounds = await Campground.find();
+  res.render("campgrounds/index", { campgrounds });
+});
+
+// REMEMBER ORDER MATTERS THATS WHY WE PUT NEW CAMPGROUND ROUTE ABOVE ID ROUTE
+//TO CREATE A FORM WE USUALLY NEED TWO ROUTES 1- THE ACTUAL FORM ITSELF AS GET 2- CREATE ROUTE AS POST
+//(4) FORM GET ROUTE
+app.get("/campgrounds/new", (req, res) => {
+  res.render("campgrounds/new");
+});
+//NOW WE SET UP THE POST ROUTE FPR THE NEW FPRM
+//(5) To parse the req.body we use express.urlencoded line above
+app.post("/campgrounds", async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
+//(3) Details page for our Campground
+app.get("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  const campgrounds = await Campground.findById(id);
+  res.render("campgrounds/show", { campgrounds });
+});
+
+//(6) Edit camps. We AGAIN NEED TWO ROUTES 1- FOR FORM  2- for SUBMITTING THAT FORM
+// (6.1) WE will use METHOD_OVERRIDE  to fake request..INSTALL IT IN NPM and then require it on top
+app.get("/campgrounds/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  res.render("campgrounds/edit", { campground });
+});
+//(6.2) Here we make a put route
+app.put("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(id, {
+    ...req.body.campground,
+  });
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
+//(7) DELETE ROUTE.. WE MAKE A FAKE FORM ACTION IN SHOW.EJS 
+app.delete("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  await Campground.findByIdAndDelete(id);
+  res.redirect('/campgrounds')
+});
+app.listen(port, () => {
+  console.log("CONNECTED TO PORT: " + port);
+});
---
+++ seeds/index.js
@@ -0,0 +1,39 @@
+//This file is self contained
+// It basically connects to mongoose and use the Campground Model
+
+const Campground = require("../models/campground");
+const mongoose = require("mongoose");
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelpers");
+
+mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection error:"));
+db.once("open", () => {
+  console.log("Database Connected!");
+});
+
+// Making a function for random place and descriptors so it looks neet
+//This helps to get random place or descriptors of an array in seedHelpers
+const sample = (arr) => {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
+
+//(1)Start by removing everything in database
+// BE CAREFUL WITH DELETEMANY( ) INSIDE THE FUNCTION
+const seedDB = async () => {
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const camp = new Campground({
+      title: `${sample(descriptors)} ${sample(places)}`,
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+    });
+    await camp.save();
+  }
+};
+seedDB();
